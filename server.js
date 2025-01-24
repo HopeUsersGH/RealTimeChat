5ab03cd06becb0bb2dbdeffad6ba1e0a7632e4f6
HopeUsersGH <flash.matsnev132@gmail.com>
2025-01-24 08:10:59 +0700

    add font, scss and part of layout

--- server.js
+++ server.js
@@ -1,10 +1,28 @@
 const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
+const fs = require('fs');
+
 const app = express();
+const server = http.Server(app);
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello, World!');
+const io = socketIO(server);
+
+let usersData = [];
+fs.readFile('./client/users.json', 'utf8', (err, data) => {
+    if (err) {
+        console.error('Error reading users data:', err);
+        return;
+    }
+    usersData = JSON.parse(data);  
 });
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/client/index.html');
+});
+
+app.use(express.static('./client'));
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
